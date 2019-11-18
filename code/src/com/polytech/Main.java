c6364b21985bf92af03e505f95db135afe0d5809
XieYuLin <xieyl_github@163.com>
2019-11-18 23:31:27 +0100

    undo unnecessary codes

--- code/src/com/polytech/Main.java
+++ code/src/com/polytech/Main.java
@@ -6,8 +6,6 @@ import java.net.UnknownHostException;
 public class Main {
 
     public static void main(String[] args) throws UnknownHostException {
-        InetAddress host = InetAddress.getLocalHost();
-        String ip =host.getHostAddress();
-        System.out.println("本机ip地址：" + ip);
+
     }
 }
