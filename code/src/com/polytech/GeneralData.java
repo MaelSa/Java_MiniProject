35339ef7aab89bb6a7883050f500d4ada1ed378f
MaelSa <43341724+MaelSa@users.noreply.github.com>
2019-12-02 12:06:52 +0100

    Starting general data class

--- code/src/com/polytech/GeneralData.java
+++ code/src/com/polytech/GeneralData.java
@@ -1,4 +1,35 @@
 package com.polytech;
 
-public class PlayersInfo {
+import java.util.Hashtable;
+
+public class GeneralData implements java.io.Serializable{
+    private Hashtable<String, Integer> players_info;
+    private Hashtable<String, Integer> songs_info;
+
+    public GeneralData(){
+        players_info = new Hashtable<String, Integer>();
+        songs_info = new Hashtable<String, Integer>();
+    }
+
+    public void add_song(String songname){
+        int size = songname.length();
+        String clean_name = songname.substring(size - 5, size - 1);
+        this.songs_info.put(clean_name, 0);
+        System.out.println("Song" + clean_name + " added to song info");
+    }
+
+    public void add_player(String player){
+        this.players_info.put(player, 0);
+    }
+
+    public void update_song_player(String song, String player){
+        String clean_song_name = song.substring(song.length() -5, song.length() - 1);
+        if (!this.players_info.contains(player)){
+            add_player(player);
+        }
+        if (!this.songs_info.contains(song)){
+            add_song(song);
+        }
+        //find a way to update both hashtables
+    }
 }
