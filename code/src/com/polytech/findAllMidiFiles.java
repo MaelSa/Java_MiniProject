fd55b69ab41bfa71d60f8da8020f4ebd0e4c5f2f
MaelSa <43341724+MaelSa@users.noreply.github.com>
2019-12-02 19:27:07 +0100

    adding findAllMidiFiles

---
+++ code/src/com/polytech/findAllMidiFiles.java
@@ -0,0 +1,26 @@
+package com.polytech;
+
+import java.io.File;
+import java.lang.reflect.Field;
+
+public class findAllMidiFiles {
+    public static void main(String args[]){
+        find(new File("code"));
+    }
+    public static void find(File file){
+
+        if(file.isDirectory()){
+            File[] temp = file.listFiles();
+            if(temp!=null)
+                for(File f:temp)
+                    find(f);
+        }
+        if(file.isFile()){
+            if("mid".equals(file.getName().substring(file.getName().lastIndexOf(".")+1).toLowerCase())){
+//                String s = file
+                System.out.println(file.getName().substring(0,file.getName().length()-4));
+            }
+        }
+    }
+
+}
