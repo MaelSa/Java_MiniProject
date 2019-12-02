e00ff88482f4f8a75f492db15e23c516ddb9acbf
XieYuLin <17522733+ogugugugugua@users.noreply.github.com>
2019-12-02 21:06:38 +0100

    modify findAllMidiFiles.java
    modify the searching program such that it returns String

--- code/src/com/polytech/findAllMidiFiles.java
+++ code/src/com/polytech/findAllMidiFiles.java
@@ -1,26 +1,28 @@
 package com.polytech;
-
 import java.io.File;
-import java.lang.reflect.Field;
 
 public class findAllMidiFiles {
     public static void main(String args[]){
-        find(new File("C:\\Users\\11437\\javaMiniProject\\Java_MiniProject\\code"));
+        String result = "";
+        result = find(new File("D:\\JavaMiniProject\\files"), "");
+        System.out.println(result);
     }
-    public static void find(File file){
 
+    public static String find(File file,String result){
         if(file.isDirectory()){
             File[] temp = file.listFiles();
             if(temp!=null)
                 for(File f:temp)
-                    find(f);
+                    find(f,result);
         }
-        if(file.isFile()){
+        else if(file.isFile()){
             if("mid".equals(file.getName().substring(file.getName().lastIndexOf(".")+1).toLowerCase())){
-//                String s = file
                 System.out.println(file.getName().substring(0,file.getName().length()-4));
+                result += file.getName().substring(0,file.getName().length()-4);
+                result += "\n";
+                return result;
             }
         }
+        return result;
     }
-
 }
