058789cffca5891c46972be62dfa5e6d0d21af68
MaelSa <43341724+MaelSa@users.noreply.github.com>
2019-12-03 18:12:43 +0100

    things

--- code/src/com/polytech/findAllMidiFiles.java
+++ code/src/com/polytech/findAllMidiFiles.java
@@ -1,14 +1,15 @@
 package com.polytech;
 import java.io.File;
+import java.util.ArrayList;
 
 public class findAllMidiFiles {
     public static void main(String args[]){
         String result = "";
-        result = find(new File("code"), "");
+        result = midiFilesString(new File("code"), "");
         System.out.println(result);
     }
 
-    public static String find(File file,String result){
+    public static String midiFilesString(File file,String result){
         if(file.isDirectory()){
             File[] temp = file.listFiles();
             if(temp!=null)
@@ -20,6 +21,23 @@ public class findAllMidiFiles {
                 System.out.println(file.getName().substring(0,file.getName().length()-4));
                 result += file.getName().substring(0,file.getName().length()-4);
                 result += "\n";
+                return result;
+            }
+        }
+        return result;
+    }
+
+    public static ArrayList<String> midiFilesArrayList(File file, ArrayList<String> result){
+        if(file.isDirectory()){
+            File[] temp = file.listFiles();
+            if(temp!=null)
+                for(File f:temp)
+                    midiFilesArrayList(f,result);
+        }
+        else if(file.isFile()){
+            if("mid".equals(file.getName().substring(file.getName().lastIndexOf(".")+1).toLowerCase())){
+                System.out.println(file.getName().substring(0,file.getName().length()-4));
+                result.add(file.getName().substring(0,file.getName().length()-4));
                 return result;
             }
         }
