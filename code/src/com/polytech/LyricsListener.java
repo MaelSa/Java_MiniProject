e5059e61f639d673af03bf1c146063d4b58a2de6
XieYuLin <17522733+ogugugugugua@users.noreply.github.com>
2019-12-04 10:38:31 +0100

    fixer la répétition de lyrics

--- code/src/com/polytech/LyricsListener.java
+++ code/src/com/polytech/LyricsListener.java
@@ -2,22 +2,43 @@ package com.polytech;
 import javax.sound.midi.*;
 import javax.sound.midi.MetaEventListener;
 import javax.sound.midi.MetaMessage;
+import java.io.*;
+import java.util.Arrays;
 
 public class LyricsListener implements MetaEventListener {
+    static Object data;
     @Override
     public void meta(MetaMessage metaMessage) {
-        System.out.println(metaMessage.getType());
         //type = 5 means lyrics
         if(metaMessage.getType() == 5) {
             String string = new String(metaMessage.getData());
-            //System.out.print(string);
+            if(!string.equals(data)){
+                System.out.print(string);
+                data = string;
+            }
         }
         // type = 6 : parts of the song, chorus bridge...
-        else if(metaMessage.getType() == 6) {
+        else if (metaMessage.getType() == 6) {
             String string = new String(metaMessage.getData());
-            System.out.println(string);
-
+            if(!string.equals(data)){
+                System.out.print(string);
+                data = string;
+            }
         }
+    }
 
+    public static void main(String[] args) {
+        InputStream is = null;
+        try {
+            is = new BufferedInputStream(new FileInputStream(new File("D:\\JavaMiniProjectMaster\\Java_MiniProject-master\\Java_MiniProject-master\\code\\Barbiegirl.mid")));
+            LyricsListener listener = new LyricsListener();
+            Sequencer sequencer = MidiSystem.getSequencer();
+            sequencer.addMetaEventListener(listener);
+            sequencer.open();
+            sequencer.setSequence(is);
+            sequencer.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
